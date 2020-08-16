89b459523769fe1f3a2c70f84dbc31d77916683e
371918642@qq.com <371918642@qq.com>
2020-08-16 13:12:33 +0800

    update

--- sys/ping/60_ping.sh
+++ sys/ping/60_ping.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
-#ip=$1
-#ip="172.26.45.178"
-count=60
+ip=$1
+#ip="www.baidu.com"
+count=30
 #localip=$(ifconfig `route | grep -v "grep" | grep '^default' | awk '{print $NF}'` | grep inet | awk '{print $2}' | head -n 1)
 localip=$(ips=`/sbin/ifconfig -a | grep -v "docker" | grep -v "veth" | grep -v "lo:" | grep -v "enp*" | grep -v "^br" | grep -v "127.0.0.1" | grep -v "172.16.0.1" | grep -v "192.168.0.1" | grep -v "172.18.0.1" | grep -v "172.19.0.1" | grep -v "172.17.0.1" | grep -v inet6 | grep "inet" | awk '{print $2}' | tr -d "addr:"`; host_name=`hostname --fqdn`; echo "${host_name}-${ips}")
 
-ip=$(ifconfig `route | grep -v "grep" | grep '^default' | awk '{print $NF}'` | grep inet | awk '{print $2}' | head -n 1)
+#ip=$(ifconfig `route | grep -v "grep" | grep '^default' | awk '{print $NF}'` | grep inet | awk '{print $2}' | head -n 1)
 step=$(basename $0 | awk -F'_' '{print $1}')
 timestamp=$(date +%s)
 tmp=ping_$ip.tmp
 
-# ping 172.26.45.178 -c 10 | grep -v "grep" | grep -v "172.26.45.178" > ping_ip.tmp
-ping $ip -c $count | grep -v "grep" | grep -v "$ip" > $tmp
+# ip="www.baidu.com";ping $ip -c 10 | grep -v "grep" | grep -v "$ip" | grep -v "icmp_seq" > ping_ip.tmp
+ping $ip -c $count | grep -v "grep" | grep -v "$ip" | grep -v "icmp_seq" > $tmp
 
-# cat ping_ip.tmp | awk -F "," 'NR==2{print $1}' | awk -F " " '{print $1}'
-#packets_transmitted=$(cat $tmp | awk -F "," 'NR==2{print $1}' | awk -F " " '{print $1}')
-
-# cat ping_ip.tmp | awk -F "," 'NR==2{print $2}' | awk -F " " '{print $1}'
-#received=$(cat $tmp | awk -F "," 'NR==2{print $2}' | awk -F " " '{print $1}')
-
-# cat ping_ip.tmp | awk -F "," 'NR==2{print $3}' | awk -F " " '{print $1}' | awk -F "%" '{print $1}'
+# cat ping_www.baidu.com.tmp | awk -F "," 'NR==2{print $3}' | awk -F " " '{print $1}' | awk -F "%" '{print $1}'
 packet_loss=$(cat $tmp | awk -F "," 'NR==2{print $3}' | awk -F " " '{print $1}' | awk -F "%" '{print $1}')
 
-# cat ping_ip.tmp | grep -v "grep" | grep "rtt" | awk '{print $4}' | awk -F '/' '{print $4}'
+# cat ping_www.baidu.com.tmp | grep -v "grep" | grep "rtt" | awk '{print $4}' | awk -F '/' '{print $4}'
 min_response_time=$(cat $tmp | grep -v "grep" | grep "rtt" | awk '{print $4}' | awk -F'/' '{print $1}')
 
 avg_response_time=$(cat $tmp | grep -v "grep" | grep "rtt" | awk '{print $4}' | awk -F'/' '{print $2}')
@@ -37,7 +31,7 @@ mdev_response_time=$(cat $tmp | grep -v "grep" | grep "rtt" | awk '{print $4}' |
 echo '[
     {
         "endpoint": "'${localip}'",
-        "tags": "",
+        "tags": "'ip=${ip}'",
         "timestamp": '${timestamp}',
         "metric": "sys.ping.packet_loss",
         "value": '${packet_loss}',
@@ -45,7 +39,7 @@ echo '[
     },
     {
         "endpoint": "'${localip}'",
-        "tags": "",
+        "tags": "'ip=${ip}'",
         "timestamp": '${timestamp}',
         "metric": "sys.ping.min_response_time",
         "value": '${min_response_time}',
@@ -53,7 +47,7 @@ echo '[
     },
     {
         "endpoint": "'${localip}'",
-        "tags": "",
+        "tags": "'ip=${ip}'",
         "timestamp": '${timestamp}',
         "metric": "sys.ping.avg_response_time",
         "value": '${avg_response_time}',
@@ -61,7 +55,7 @@ echo '[
     },
     {
         "endpoint": "'${localip}'",
-        "tags": "",
+        "tags": "'ip=${ip}'",
         "timestamp": '${timestamp}',
         "metric": "sys.ping.max_response_time",
         "value": '${max_response_time}',
@@ -69,12 +63,13 @@ echo '[
     },
     {
         "endpoint": "'${localip}'",
-        "tags": "",
+        "tags": "'ip=${ip}'",
         "timestamp": '${timestamp}',
         "metric": "sys.ping.mdev_response_time",
         "value": '${mdev_response_time}',
         "step": '${step}'
     }
+
 ]'
 
-rm -rf $tmp
+echo "" > $tmp
